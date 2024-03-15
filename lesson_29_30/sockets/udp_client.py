2d5663240f9ef1a59f5b1c6d810b461cf2b9254d
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2024-03-16 00:34:59 +0100

    hw30 udp server&client + cipher all-in-one

--- lesson_29_30/sockets/udp_client.py
+++ lesson_29_30/sockets/udp_client.py
@@ -4,34 +4,38 @@ from time import sleep
 
 HOST = "localhost"
 PORT = 8080
+C_PORT = 8085
 
 client_s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-client_s.bind((HOST, 8081))
+client_s.bind((HOST, C_PORT))
 
 
-def handle_responses():
+def handle_requests():
     while True:
         sleep(0.2)
-        msg = input("msg: ").encode()
+        msg = input(f"{C_PORT} msg: ").encode()
+        client_s.sendto(msg, (HOST, PORT))
 
         if msg == b"exit":
+            sleep(1)
             client_s.shutdown(1)
             client_s.close()
             break
 
-        client_s.sendto(msg, (HOST, PORT))
 
-
-thread = threading.Thread(target=handle_responses, daemon=True)
+thread = threading.Thread(target=handle_requests, daemon=True)
 thread.start()
 
 
 while True:
+    if not thread.is_alive():
+        break
     try:
         resp, addr = client_s.recvfrom(1024)
 
         print(f">>> Response: {resp.decode()}")
         print(f"from {addr}")
-    except OSError:
+    except OSError as e:
         print("connection closed")
+        print(e)
         break
--- lesson_29_30/sockets/udp_server.py
+++ lesson_29_30/sockets/udp_server.py
@@ -7,11 +7,14 @@ HOST = "localhost"
 PORT = 8080
 
 MSG = b"Well received"
-INIT_MSG = b"Send 'Start' to start the game"
+INIT_MSG = "Send 'Start' to start the game or 'Cipher' to start message encryption"
 GAME_MSG = b"Guess the number 1-10 or send 'Stop' to end"
-WRONG_TYPE_MSG = b"What you sent is not a number"
-WIN_MSG = "CONGRATS \U0001F920".encode()
+GAME_START_MSG = "has started the GAME"
+WRONG_TYPE_MSG = "What you sent is not a number"
+WIN_MSG = "CONGRATS \U0001F920"
 WRONG_NUMBER = b"Try harder"
+WELCOME_MSG = "has joined the game"
+STOP_MSG = "has stopped the game"
 
 number = random.randint(1, 10)
 
@@ -21,50 +24,175 @@ server_s.bind((HOST, PORT))
 print(f"Server is listening on {HOST}: {PORT}")
 
 start_game = False
-current_addrs = []
+start_encryption = False
+users = {"user_count": 0}
+
+user_names = [
+    "honey_bunny",
+    "lazy_dog",
+    "sweet_fury",
+    "mr_Bond",
+]
 
 
 def receive_msg() -> tuple:
-    data, addr = server_s.recvfrom(1024)
-    print(f"server received msg from {addr}")
-    server_s.sendto(MSG, addr)
-    return data.decode(), addr
+    try:
+        data, addr = server_s.recvfrom(1024)
+        print(f"server received msg from {addr}")
+        if data != b"exit":
+            server_s.sendto(MSG, addr)
+
+        return data.decode(), addr
+    except ConnectionResetError:
+        return b"exit", None
 
 
-while True:
-    data, addr = receive_msg()
+def play_game():
+    print("game started")
+    while True:
+        print(len(users))
+        if len(users) == 1:
+            return
 
-    if addr not in current_addrs:
-        current_addrs.append(addr)
-
-    if data.lower() == "start":
-        start_game = True
-        server_s.sendto(GAME_MSG, addr)
-
-    while start_game:
         data, addr = receive_msg()
 
-        if data.lower() == "stop":
-            start_game = False
-            server_s.sendto(INIT_MSG, addr)
-            break
+        if data == "exit":
+            print(f"exit {users}")
+            if addr:
+                users.pop(addr)
+            continue
 
-        if addr not in current_addrs:
-            current_addrs.append(addr)
+        if data.lower() == "stop":
+            for a in users:
+                if a != "user_count":
+                    msg = users[a] + " " + STOP_MSG
+                    server_s.sendto(msg.encode(), a)
+
+            return
+
+        if addr not in users:
+            users[addr] = user_names[users["user_count"]]
+            users["user_count"] += 1
+            print(users)
             server_s.sendto(GAME_MSG, addr)
+            for a in users:
+                if a != "user_count":
+                    msg = users[a] + " " + WELCOME_MSG
+                    server_s.sendto(msg.encode(), a)
+
             continue
 
         try:
             if int(data) == number:
-                server_s.sendto(WIN_MSG, addr)
-                start_game = False
+                msg = users[addr] + ", " + WIN_MSG
+                for a in users:
+                    if a != "user_count":
+                        server_s.sendto(msg.encode(), a)
+
+                return
             else:
                 server_s.sendto(WRONG_NUMBER, addr)
 
         except ValueError:
-            server_s.sendto(WRONG_TYPE_MSG, addr)
+            msg = users[addr] + " " + WRONG_TYPE_MSG
+            for a in users:
+                if a != "user_count":
+                    server_s.sendto(msg.encode(), a)
 
-    server_s.sendto(INIT_MSG, addr)
+
+def procced_encryption(msg: str, salt: int) -> str:
+    enc_msg = []
+    for i in msg:
+        i = chr(ord(i) + salt)
+        enc_msg.append(i)
+
+    return "".join(enc_msg)
+
+
+def encrypt_msg(enc_addr):
+
+    while True:
+        server_s.sendto("Send a number to start encryption of the messages".encode(), enc_addr)
+        data, addr = receive_msg()
+
+        if addr != enc_addr:
+            continue
+
+        try:
+            salt = int(data)
+            server_s.sendto("number accepted".encode(), addr)
+        except ValueError:
+            server_s.sendto(WRONG_TYPE_MSG.encode(), addr)
+            continue
+
+        break
+
+    while True:
+        data, addr = receive_msg()
+
+        if data == "exit":
+            print(f"exit {users}")
+            if addr:
+                for a in users:
+                    if a != "user_count":
+                        server_s.sendto(f"{users[a]} has left".encode(), a)
+                users.pop(addr)
+
+            if addr == enc_addr:
+                return
+
+            continue
+
+        if addr == enc_addr:
+            data = procced_encryption(data, salt)
+
+        for a in users:
+            if a != "user_count":
+                server_s.sendto(data.encode(), a)
+
+while True:
+    data, addr = receive_msg()
+
+    if data == "exit":
+        print(f"exit {users}")
+        if addr:
+            for a in users:
+                if a != "user_count":
+                    server_s.sendto(f"{users[a]} has left".encode(), a)
+            users.pop(addr)
+        continue
+
+    if addr not in users:
+        users[addr] = user_names[users["user_count"]]
+        users["user_count"] += 1
+        print(users)
+
+    if data.lower() == "start":
+        start_game = True
+
+    if data.lower() == "cipher":
+        start_encryption = True
+
+    if start_game:
+        msg = users[addr] + " " + GAME_START_MSG
+        print(msg)
+
+        for a in users:
+            print(users)
+            print(a)
+
+            if a != "user_count":
+                server_s.sendto(msg.encode(), a)
+                server_s.sendto(GAME_MSG, a)
+
+        play_game()
+        start_game = False
+
+    if start_encryption:
+        encrypt_msg(addr)
+        start_encryption = False
+
+    server_s.sendto(INIT_MSG.encode(), addr)
 
 
 
